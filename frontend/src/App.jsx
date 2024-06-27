e2b0423f6c87547d2b89c2e95e935abf96155fe8
VishnuSanal <t.v.s10123@gmail.com>
2024-06-27 15:33:52 +0530

    dash ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
 
+import { useNavigate } from "react-router-dom";
+
+import Books from './Books'
+
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+  let [target, setTarget] = useState(['Books', 'Issues', 'Members'])
+
   return (
     <>
       <Card>
@@ -17,9 +26,27 @@ function App() {
         <CardContent>
 
           {
-            ['Books', 'Issues', 'Members']
+            target
               .map(item =>
-                <Card className='card'>
+                <Card className='card'
+                  key={item}
+                  onClick={function () {
+
+                    switch (item) {
+                      case 'Books':
+                        setTarget(['Import Books'])
+                        break;
+                      case 'Issues':
+                        setTarget(['New Issue'])
+                        break;
+                      case 'Members':
+                        setTarget(['Add Members'])
+                        break;
+                      default:
+                        setTarget(['Books', 'Issues', 'Members'])
+                    }
+
+                  }}>
                   <CardHeader>
                     <CardTitle>{item}</CardTitle>
                   </CardHeader>
