778deeec8a2f37bbeb1460259e1c45010baa6c07
VishnuSanal <t.v.s10123@gmail.com>
2024-06-28 19:57:50 +0530

    temp update

--- frontend/src/Books.jsx
+++ frontend/src/Books.jsx
@@ -15,6 +15,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
@@ -23,11 +24,14 @@ import { useEffect, useState } from 'react';
 import { Button } from './components/ui/button';
 import { Label } from "./components/ui/label";
 import { Input } from "./components/ui/input";
+import { Checkbox } from "./components/ui/checkbox";
 
 function Books() {
 
   const [title, setTitle] = useState("Book List");
   const [books, setBooks] = useState([]);
+
+  const [selectedItems, setSelectedItems] = useState([]);
 
   useEffect(() => {
     fetch('https://library-app-6cyw.onrender.com/api/v1/book/')
@@ -42,12 +46,18 @@ function Books() {
       });
   }, [])
 
-  let list;
+  let content;
 
   if (books.length > 0) {
-    list = books.map(book =>
+    content = books.map(book =>
       <div key={book.isbn}>
-        <Card>
+        <Card onClick={function (event) {
+          if (!selectedItems.some(e => e.bookID === book.bookID)) {
+            setSelectedItems(selectedItems => [...selectedItems, book])
+          } else {
+            setSelectedItems(selectedItems.filter(e => e.bookID !== book.bookID))
+          }
+        }}>
           <CardHeader className="list-card">
             <CardTitle>{book.authors}</CardTitle>
             <CardDescription>{book.title}</CardDescription>
@@ -56,12 +66,10 @@ function Books() {
       </div>
     )
   } else {
-    list = <div>
-      <CardDescription>List Empty</CardDescription>
+    content = <div>
+      <CardDescription>{title} Empty</CardDescription>
     </div>
   }
-
-  const [formValue, setFormValue] = useState({});
 
   return (
     <>
@@ -70,83 +78,133 @@ function Books() {
           <CardTitle>{title}</CardTitle>
         </CardHeader>
         <CardContent>
-          <div>{list}</div>
-
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button variant="ghost" className="button">Import Books</Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Search</DialogTitle>
-                <DialogDescription>
-                  Enter the search keywords. Leave empty if not applicable
-                </DialogDescription>
-              </DialogHeader>
-
-              <div className="grid gap-4 py-4">
-                {
-                  ['Title', 'Authors', 'ISBN', 'Publisher', 'Page']
-                    .map(currentItem =>
-                      <div className="grid grid-cols-4 items-center gap-4" key={currentItem}>
-                        <Label htmlFor={currentItem.toLowerCase()} className="text-right">
-                          {currentItem}
-                        </Label>
-                        <Input
-                          id={currentItem.toLowerCase()}
-                          defaultValue=""
-                          onChange={function (e) {
-                            setFormValue({ ...formValue, [currentItem.toLowerCase()]: e.target.value })
-                          }}
-                          className="col-span-3"
-                        />
-                      </div>
-                    )
-                }
-              </div>
-
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button type="submit" onClick={function (event) {
-
-                    let link = "https://library-app-6cyw.onrender.com/api/v1/fetch?"
-
-                    {
-                      ['title', 'authors', 'isbn', 'publisher', 'page']
-                        .map(value => {
-                          if (formValue[value] !== undefined)
-                            link += value + "=" + formValue[value] + "&"
-                        }
-                        )
-                    }
-
-                    {
-                      fetch(link)
-                        .then((response) => {
-                          return response.json();
-                        })
-                        .then((data) => {
-                          setTitle("Search Results")
-                          setBooks(data);
-                        })
-                        .catch((error) => {
-                          console.log(error);
-                        });
-                    }
-
-                  }
-                  }>
-                    Search
-                  </Button>
-                </DialogClose>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-
-
+          <div>{content}</div>
         </CardContent>
+        <CardFooter>
+          <div>
+            < SearchBooksDialog setTitle={setTitle} setBooks={setBooks} /> < ImportBooksDialog setTitle={setTitle} selectedItems={selectedItems} />
+          </div>
+        </CardFooter>
       </Card>
     </>
+  )
+}
+
+const ImportBooksDialog = ({ setTitle, selectedItems }) => {
+
+  const [formValue, setFormValue] = useState({});
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost" className="button">Import Books</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Search</DialogTitle>
+          <DialogDescription>
+            Enter the search keywords. Leave empty if not applicable
+          </DialogDescription>
+        </DialogHeader>
+
+        {/* {console.log(selectedItems)} */}
+
+        <div>
+
+        </div>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="submit" onClick={function (event) {
+              setTitle("Book List")
+            }
+            }>
+              Submit
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+
+  )
+}
+
+const SearchBooksDialog = ({ setTitle, setBooks }) => {
+
+  const [formValue, setFormValue] = useState({});
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="ghost" className="button">Search Books</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Search</DialogTitle>
+          <DialogDescription>
+            Enter the search keywords. Leave empty if not applicable
+          </DialogDescription>
+        </DialogHeader>
+
+        <div className="grid gap-4 py-4">
+          {
+            ['Title', 'Authors', 'ISBN', 'Publisher', 'Page']
+              .map(currentItem =>
+                <div className="grid grid-cols-4 items-center gap-4" key={currentItem}>
+                  <Label htmlFor={currentItem.toLowerCase()} className="text-right">
+                    {currentItem}
+                  </Label>
+                  <Input
+                    id={currentItem.toLowerCase()}
+                    defaultValue=""
+                    onChange={function (e) {
+                      setFormValue({ ...formValue, [currentItem.toLowerCase()]: e.target.value })
+                    }}
+                    className="col-span-3"
+                  />
+                </div>
+              )
+          }
+        </div>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="submit" onClick={function (event) {
+
+              let link = "https://library-app-6cyw.onrender.com/api/v1/fetch?"
+
+              {
+                ['title', 'authors', 'isbn', 'publisher', 'page']
+                  .map(value => {
+                    if (formValue[value] !== undefined)
+                      link += value + "=" + formValue[value] + "&"
+                  }
+                  )
+              }
+
+              {
+                fetch(link)
+                  .then((response) => {
+                    return response.json();
+                  })
+                  .then((data) => {
+                    setTitle("Search Results")
+                    setBooks(data);
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });
+              }
+
+            }
+            }>
+              Search
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+
   )
 }
 
