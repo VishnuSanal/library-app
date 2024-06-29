4987990011c0ff1157138ccb5ee35bb8db86dac7
VishnuSanal <t.v.s10123@gmail.com>
2024-06-30 00:14:51 +0530

    implement put methods

--- backend/app.py
+++ backend/app.py
@@ -68,8 +68,70 @@ class Books(Resource):
 
         book = Book.create_from_data(args)
 
-        if Book.query.filter_by(bookID=args['bookID']).first():
-            Book.query.filter_by(bookID=args['bookID']).delete()
+        db.session.add(book)
+        db.session.commit()
+
+        return Book.query.all()
+
+    @marshal_with(book_fields)
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("bookID", help="bookID is required", type=str, required=True, trim=True)
+
+        parser.add_argument("title", help="title is required", type=str, required=False, trim=True)
+        parser.add_argument("authors", help="authors is required", type=str, required=False, trim=True)
+        parser.add_argument("average_rating", help="average_rating is required", type=str, required=False, trim=True)
+        parser.add_argument("isbn", help="isbn is required", type=str, required=False, trim=True)
+        parser.add_argument("isbn13", help="isbn13 is required", type=str, required=False, trim=True)
+        parser.add_argument("language_code", help="language_code is required", type=str, required=False, trim=True)
+        parser.add_argument("ratings_count", help="ratings_count is required", type=str, required=False, trim=True)
+        parser.add_argument("text_reviews_count", help="text_reviews_count is required", type=str, required=False,
+                            trim=True)
+        parser.add_argument("publication_date", help="publication_date is required", type=str, required=False,
+                            trim=True)
+        parser.add_argument("publisher", help="publisher is required", type=str, required=False, trim=True)
+        parser.add_argument("book_count", help="book_count is required", type=str, required=False, trim=True)
+
+        parser.add_argument("  num_pages", help="num_pages is required", type=str, required=False, trim=True)
+        parser.add_argument("num_pages", help="num_pages is required", type=str, required=False, trim=True)
+
+        args = parser.parse_args()
+
+        existing_book = Book.query.filter_by(bookID=args['bookID']).first()
+
+        if not existing_book:
+            abort(400, message="No such book")
+
+        Book.query.filter_by(bookID=args['bookID']).delete()
+
+        book = Book.create_from_data(existing_book.__dict__)
+
+        if args['title']:
+            book.title = args['title']
+        if args['authors']:
+            book.authors = args['authors']
+        if args['average_rating']:
+            book.average_rating = args['average_rating']
+        if args['isbn']:
+            book.isbn = args['isbn']
+        if args['isbn13']:
+            book.isbn13 = args['isbn13']
+        if args['language_code']:
+            book.language_code = args['language_code']
+        if args['ratings_count']:
+            book.ratings_count = args['ratings_count']
+        if args['text_reviews_count']:
+            book.text_reviews_count = args['text_reviews_count']
+        if args['publication_date']:
+            book.publication_date = args['publication_date']
+        if args['publisher']:
+            book.publisher = args['publisher']
+        if args['book_count']:
+            book.book_count = args['book_count']
+        if args['num_pages']:
+            book.num_pages = args['num_pages']
+        if args['  num_pages']:
+            book.num_pages = args['  num_pages']
 
         db.session.add(book)
         db.session.commit()
@@ -146,6 +208,37 @@ class Members(Resource):
         member = Member(name=args.get('name'))
 
         db.session.add(member)
+        db.session.commit()
+
+        member_list = Member.query.all()
+
+        today = dateutil.parser.parse(datetime.today().strftime('%d/%m/%Y'))
+
+        for entry in member_list:
+            amount_due = 0
+
+            for date in entry.issue_dates:
+                issue_date = dateutil.parser.parse(date)
+                amount_due += (today - issue_date).days * 10
+
+            entry.amount_due = amount_due
+
+        return member_list
+
+    @marshal_with(member_fields)
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("id", type=str, required=True, trim=True, help="id is required")
+        parser.add_argument("name", type=str, required=True, trim=True, help="Name is required")
+        args = parser.parse_args()
+
+        member_query = Member.query.filter_by(id=args['id'])
+
+        if not member_query.first():
+            abort(400, message="No such member")
+
+        member_query.update({'name': args['name']})
+
         db.session.commit()
 
         member_list = Member.query.all()
